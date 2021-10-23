8259c3268e94f2b3ee8279b675afd9a3ab0f9fe8
Darpan Chavhan <darpanc@hyperlinkinfosystem.net.in>
2021-10-24 02:06:27 +0530

    In component.ts created all the properties which gonna be use in the html file.

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  
+
+  // ############ Example for interpolation start #######//
+
+  // Strings
+  // name ="daps chavhan";
+
+  // Functions 
+  // test () {
+    // return "holla everyone,"
+
+    // If we wanna use existing variable or objects data then 
+    // return "Hello" +" "+ this.name;
+  // }
+
+  // objects 
+  // obj =  {
+    // email : "daps.chavhan@yahoo.com",
+    // phone_number : 9099081533,
+  // }
+
+  // array data :: 
+  // hobbies = ["football", "badminton","chess","cricket"];
+
+  // Total amount
+  // product1 = 100;
+  // product2 = 200;
+
+  // current page's url 
+  // siteUrl = window.location.href;
+
+  // ############ Example for interpolation end #######//
 }
