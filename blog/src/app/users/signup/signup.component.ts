e81d4bddd16dfcb55aa153ebf552b749499aa968
Darpan Chavhan <darpanc@hyperlinkinfosystem.net.in>
2021-10-24 17:34:41 +0530

    Component.ts file generally use for the all the function which we gonna use in the component.

--- blog/src/app/users/signup/signup.component.ts
+++ blog/src/app/users/signup/signup.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+   
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  yourName ="john doe"
+  //  onclick call function .
+  getName(name: any){
+    console.log("User name is ::"+name)
+  }
+
+  // my event
+  myEvent (params:any) {
+    console.log(params)
+  }
+  
+  // text box values 
+  currentVal = "";
+  getVal (val : any) {
+    // this.currentVal = val.target.value;
+    // console.log(val.target.value)
+
+    this.currentVal = val;
+    
+  }
 }
