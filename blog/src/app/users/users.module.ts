749f97dc2a2df7f26291b74ef59016bf7a272480
Darpan Chavhan <darpanc@hyperlinkinfosystem.net.in>
2021-10-30 15:56:51 +0530

    added new component

--- blog/src/app/users/users.module.ts
+++ blog/src/app/users/users.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    FormDemoComponent
   ],
   imports: [
     CommonModule
   ],
   exports : [
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    FormDemoComponent
   ]
 })
 export class UsersModule { }
