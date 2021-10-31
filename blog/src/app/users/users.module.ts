9f2eaf576ab19d345539d278492253eab7ee0496
Darpan Chavhan <darpanc@hyperlinkinfosystem.net.in>
2021-10-31 17:58:35 +0530

    - Added new modules which is gonna be use in the input forms.

--- blog/src/app/users/users.module.ts
+++ blog/src/app/users/users.module.ts
@@ -3,25 +3,34 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { FormDemoComponent } from './form-demo/form-demo.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { StyleBindingComponent } from './style-binding/style-binding.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     LoginComponent,
     SignupComponent,
-    FormDemoComponent
+    FormDemoComponent,
+    StyleBindingComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    NgbModule,
+    MatButtonModule,
+    MatInputModule
   ],
   exports : [
     LoginComponent,
     SignupComponent,
-    FormDemoComponent
+    FormDemoComponent,
+    StyleBindingComponent
   ]
 })
 export class UsersModule { }
